2559ac7cede5f4e80962697d4344f2c1f2dcab73
Ксения Ладошко <87228073+KsushaLad@users.noreply.github.com>
2021-12-07 15:52:38 +0200

    Delete app/src/main/java/java/com/codinginflow/imagesearchapp directory

--- app/src/main/java/java/com/codinginflow/imagesearchapp/application/ImageSearchApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.codinginflow.imagesearchapp.application
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ImageSearchApplication : Application()
